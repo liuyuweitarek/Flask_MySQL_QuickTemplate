22283c5809816d075e63a8d8689c517c5524cb21
liuyuweitarek <liuyuwei.tarek@gmail.com>
2022-03-12 09:13:34 +0800

    add requirements.txt

--- cfg.py
+++ cfg.py
@@ -1,12 +1,12 @@
 class ProductionConfig:
     # Edit from here:
-    DEBUF = False
+    DEBUG = False
     HOST = {YOUR IP ADDRESS}
-    PORT = 17300
+    PORT = {PORT which is available on your machine}
     SECRET_KEY = "Ane;ri-qlklqe/dw?ckjnwl_lw"
 
     # SQL
-    SQLALCHEMY_DATABASE_URI= "mysql://{ACCOUNT}:{PASSWORD}@127.0.0.1/{DATABASE_NAME}"
+    SQLALCHEMY_DATABASE_URI= "mysql://{ACCOUNT}:{PASSWORD}@127.0.0.1/{DATABASE_NAME}_prod"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MYSQL_DATABASE_CHARSET = 'utf8mb4'
     AUTHY_API_KEY = "r1jjy98p51MSFQT681w99LDW3XCjezt6"
@@ -16,11 +16,11 @@ class DevelopmentConfig:
     DEBUG = True
     # Flask
     HOST = {YOUR IP ADDRESS}
-    PORT = 17400
+    PORT = {PORT which is available on your machine}
     SECRET_KEY = "Sln;uh-pl;lOP/dw?ckjnwl_qq"
     
     # SQL
-    SQLALCHEMY_DATABASE_URI= "mysql://{ACCOUNT}:{PASSWORD}@127.0.0.1/{DATABASE_NAME}"
+    SQLALCHEMY_DATABASE_URI= "mysql://{ACCOUNT}:{PASSWORD}@127.0.0.1/{DATABASE_NAME}_dev"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MYSQL_DATABASE_CHARSET = 'utf8mb4'
 
